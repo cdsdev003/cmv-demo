c90fa67c66637a40128077a554d115b7942cbf71
cds.dev003 <cds.dev003@gmail.com>
2023-02-14 16:06:35 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,112 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Date and time picker
+    $('.date').datetimepicker({
+        format: 'L'
+    });
+    $('.time').datetimepicker({
+        format: 'LT'
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        loop: true,
+        nav: false,
+        dots: true,
+        items: 1,
+        dotsData: true,
+    });
+
+
+    // Testimonials carousel
+    $('.testimonial-carousel').owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        loop: true,
+        nav: false,
+        dots: true,
+        items: 1,
+        dotsData: true,
+    });
+
+    
+})(jQuery);
+
+
+let tinySdlier = () => {
+  let propertySlider = document.querySelectorAll(".property-slider");
+  if (propertySlider.length > 0) {
+    let tnsSlider = tns({
+      container: ".property-slider",
+      mode: "carousel",
+      speed: 700,
+      gutter: 30,
+      items: 3,
+      autoplay: true,
+      autoplayButtonOutput: false,
+      controlsContainer: "#property-nav",
+      responsive: {
+        0: {
+          items: 1,
+        },
+        700: {
+          items: 2,
+        },
+        900: {
+          items: 3,
+        },
+      },
+    });
+  }
+};
+tinySdlier();
